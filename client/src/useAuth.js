261ecdacb4aaf852034aaa5753c5dd9d42f3d928
Simi <szeiger03@gmail.com>
2023-03-13 13:43:38 -0400

    third commit

---
+++ client/src/useAuth.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+
+export default function useAuth(code) {
+    const [accessToken, setAccessToken] = useState()
+    const [refreshToken, setRefreshToken] = useState()
+    const [expiresIn, setExpiresIn] = useState()
+
+useEffect(() => {
+    effect
+}, [code])
+}
