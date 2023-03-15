2e638f60540a3f1a3c67aef26bde8fdf0077ba6c
Simi <szeiger03@gmail.com>
2023-03-15 12:02:08 -0400

    adding style to the page

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
 import { useAuth } from './useAuth';
 import { container, Form } from 'react-bootstrap'
 
 export default function Dashboard({code }) {
     const accessToken = useAuth(code)
-    return <container>
-        <Form.Control type='search' placeholder='Search Songs/
-         Artists' value={search} onChange={(e) => setSearch(e.target.value)} />
-        </container>
+    const [search,setSearch] = useState('')
+    return( 
+    <container className='d-flex flex-column py-2' style={
+      {height: '100vh'}}>
+        <Form.Control 
+        type='search' 
+        placeholder='Search Songs/Artists' 
+        value={search} 
+        onChange={(e) => setSearch(e.target.value)} 
+        />
+        <div className='flex-grow-1 my-2' style={{ overflowY: 'auto'}}>
+            Songs
+        </div>
+      </container>
+ )
 }
