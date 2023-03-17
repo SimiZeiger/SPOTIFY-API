ba44282dc9110265a40e36fc54c7b8c6fbd1e5c9
Simi <szeiger03@gmail.com>
2023-03-16 23:22:07 -0400

    created .env file

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const lyricsFinder = require('./lyricsFinder');
 const SpotifyWebApi = require('spotify-web-api-node');
+
 
 
 const app = express();
 app.use(cors())
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: '21efd09454c5498a81e7e7b20bebb1b0',
-        clientSecret: '6251019b44c04f0e90121f86dea45e0f'
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
         refreshToken,
     })
 
@@ -35,9 +39,9 @@ spotifyApi
 app.post('login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: '21efd09454c5498a81e7e7b20bebb1b0',
-        clientSecret: '6251019b44c04f0e90121f86dea45e0f'
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
     })
     spotifyApi.authorizationCodeGrant(code).then(data => {
         res.json({
@@ -46,9 +50,13 @@ app.post('login', (req, res) => {
             expiresIn: data.body.expires_in
         })
     }).catch(err => {
-        console.log(err)
         res.sendStatus(400)
     })
+})
+
+app.get('/lyrics', async (req, res) => {
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track) || 'No lyrics found'
+    res.json(lyrics)
 })
 
 app.listen(3001)
